62ee97f8852b940d932d1e3a2f0ed09a88d4754b
KMillionaireInc <hello@kmillionaireinc.com>
2025-10-23 13:16:00 -0400

    Update submit.js

--- api/submit.js
+++ api/submit.js
@@ -5,12 +5,13 @@ export default async function handler(req, res) {
 
   try {
     const SUPABASE_URL = "https://gmsjqunfsffjiksffvem.supabase.co";
-    const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imdtc2pxdW5mc2Zmamlrc2ZmdmVtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjA4NzIxNTgsImV4cCI6MjA3NjQ0ODE1OH0.7qEXKk8f8Sd3G2y8KyQ0XbeCyG8ieD-8WV18-_JkRGc";
+    const SUPABASE_KEY =
+      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imdtc2pxdW5mc2Zmamlrc2ZmdmVtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjA4NzIxNTgsImV4cCI6MjA3NjQ0ODE1OH0.7qEXKk8f8Sd3G2y8KyQ0XbeCyG8ieD-8WV18-_JkRGc";
 
     const userData = {
       first_name: req.body.first_name,
       last_name: req.body.last_name,
-      site_name: req.body.store_name,       // THIS WORD MUST MATCH your database "site_name"
+      site_name: req.body.store_name,
       subdomain: req.body.subdomain,
       email: req.body.email,
       amazon_id: req.body.amazon_id,
@@ -34,7 +35,7 @@ export default async function handler(req, res) {
 
     return res.status(200).json({ success: true });
   } catch (error) {
-    console.error("Proxy error:", error);
+    console.error("Server error:", error);
     return res.status(500).json({ error: "Internal server error" });
   }
 }
